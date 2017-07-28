249a8b4c547d74ea5c61599fd9efc25a2cd6e928
Gracie <grace@ugraphix.com>
2017-07-27 19:22:07 -0700

    WordPress Prototype Theme Creation
    WordPress Prototype Theme Creation. Still needs some tweaking to make it responsive.

--- templates/index.php
+++ templates/index.php
@@ -153,11 +153,15 @@ function hasScrolled() {
 
 
 <div id="main">
-    <h2>Main Section</h2>
+    
     <?php if (have_posts()): while (have_posts()): the_post(); ?>
-            
+        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>    
             
     <?php the_content(''); ?>
+    
+    
+    
+    
     <?php endwhile; endif; ?>
 </div>
 <div id="aside">
