e531b9cf4777f9ea101ea792411055412c170969
Gracie <grace@ugraphix.com>
2017-08-10 16:33:52 -0700

    Add a Widget to Your Site in Your Sidebar

--- templates/functions.php
+++ templates/functions.php
@@ -14,6 +14,15 @@ License URI:
 General comments: I hope this works.
 */
 
+//Register My Sidebar
+register_sidebar (array(
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h2>',
+    'after_title' => '</h2>',
+
+));
+
 //register menus
 register_nav_menus(array('main-menu' => 'Main Menu'));
 //
@@ -25,5 +34,5 @@ add_theme_support( 'post-thumbnails' );
 //Create Page Excerpts
 add_post_type_support('page', 'excerpt');
     
-    
+
 ?>
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -6,4 +6,10 @@
 			wp_list_pages(array('sort_column' => 'post_date', 'sort_order' => 'desc'));
 			?>
 		</ul>    
+    
+    <!---begin dynamic sidebar-->
+    <?php dynamic_sidebar(1);//call our widgets ?>
+    
+    <!---end dynamic sidebar-->
+    
 	</div>
